9079ef7911c4acf0a88fb9794f9b3fe2c2841b28
timmy-turner005 <timitalented005@gmail.com>
2025-10-22 18:08:10 +0100

    commit

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -46,6 +46,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
